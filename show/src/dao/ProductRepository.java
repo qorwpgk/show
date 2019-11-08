182d95aa40a7e10076577f503bfb9d923e74b2df
qorwpgk <48539270+qorwpgk@users.noreply.github.com>
2019-11-08 16:55:11 +0900

    Update ProductRepository.java

--- show/src/dao/ProductRepository.java
+++ show/src/dao/ProductRepository.java
@@ -20,15 +20,16 @@ public class ProductRepository{
 		phone.setUnitsInStock(1000);
 		phone.setCondition("new");
 		
-		Product notebook = new Product("P1235", "LG PC ±×·¥ ", 150000);
-		notebook.setDescription("13.3-inch, IPS leD HD display,8-megapixel iSinght Camera");
+		Product notebook = new Product("P1235", "LG PC ê·¸ëž¨ ", 150000);
+		notebook.setDescription("13.3-inch, IPS LED display,5rd Generation"
+				+ "Intel Core proceseeors");
 		notebook.setCategory("notebook");
 		notebook.setManufacturer("LG");
 		notebook.setUnitsInStock(1000);
 		notebook.setCondition("Refurbished");
 		
 		Product tablet = new Product("P1236", "galaxy Tab S", 90000);
-		tablet.setDescription("13.3-inch, 1234*750 Renina HD display,8-megapixel iSinght Camera");
+		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display,Octa-Core proceddor");
 		tablet.setCategory("Tablet");
 		tablet.setManufacturer("Samsung");
 		tablet.setUnitsInStock(1000);
@@ -47,7 +48,7 @@ public class ProductRepository{
 		for(int i=0; i < listOfProducts.size(); i++){
 			Product product = listOfProducts.get(i);
 			if(product != null && product.getProductId() != null &&
-					product.getProductId().equals(ProductById)){
+					product.getProductId().equals(productId)){
 				ProductById = product;
 				break;
 			}
